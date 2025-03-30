ceee245ead1909b9812b2c968fb22bbbcc08e4bf
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2025-03-29 23:32:30 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ const nextConfig: NextConfig = {
   i18n: {
     locales: ['en', 'es'],
     defaultLocale: 'es',
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      net: false, // Ignore 'net' module
+      fs: false,  // Ignore 'fs' module
+      dns: false, // Ignore 'dns' module
+    };
+    return config;
   }
+
   /* config options here */
 };
 
