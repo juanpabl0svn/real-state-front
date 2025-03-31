a4ed54a941c9ec778a3474ac574fce8b62ed3775
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2025-03-31 01:25:04 -0500

    store added

---
+++ src/stores/app-store.ts
@@ -0,0 +1,13 @@
+import { AppStore } from '@/types';
+import { create } from 'zustand';
+
+export const useAppStore = create<AppStore>((set) => ({
+  tab: null,
+  setTab: (tab) => set(() => ({ tab })),
+  isLoading: false,
+  setLoading: (isLoading) => set(() => ({ isLoading })),
+  otpCode: null,
+  setOtpCode: (otpCode) => set(() => ({ otpCode })),
+  data: null,
+  setData: (data) => set(() => ({ data })),
+}));
