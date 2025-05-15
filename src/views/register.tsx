30dc9ea80301fdb3aed567df5d7230b5caa59f1d
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2025-05-14 23:57:23 -0500

    feat: translatinos

--- src/views/register.tsx
+++ src/views/register.tsx
@@ -1,15 +1,19 @@
-import { RegisterForm } from "@/components/register-form"
+import { RegisterForm } from "@/components/register-form";
+import { useTranslations } from "next-intl";
 
 export default function RegisterPage() {
+  const t = useTranslations("auth");
   return (
     <div className="w-full flex h-screen items-center justify-center py-12">
       <div className="mx-auto w-full max-w-md space-y-6 py-12 mt-10">
         <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold">Create an account</h1>
-          <p className="text-muted-foreground">Enter your information to get started</p>
+          <h1 className="text-3xl font-bold">{t('create_an_account')}</h1>
+          <p className="text-muted-foreground">
+            {t('create_an_account_description')}
+          </p>
         </div>
         <RegisterForm />
       </div>
     </div>
-  )
+  );
 }
